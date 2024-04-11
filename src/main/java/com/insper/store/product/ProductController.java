6f9345e9c2d58956588e79d4c6c7b051d181273a
thomaschiari <thomaschiari@gmail.com>
2024-04-10 22:35:26 -0300

    update swagger

--- src/main/java/com/insper/store/product/ProductController.java
+++ src/main/java/com/insper/store/product/ProductController.java
@@ -11,27 +11,32 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 // import org.springframework.web.bind.annotation.RequestHeader;
 
+import io.swagger.annotations.ApiOperation;
 
 
 @FeignClient(name = "store-product")
 public interface ProductController {
 
+    @ApiOperation(value = "Create a new product")
     @PostMapping("/products")
     public ResponseEntity<ProductOut> create(
         @RequestBody(required = true) ProductIn in
     );
 
+    @ApiOperation(value = "Update a product by id")
     @PutMapping("/products/{id}")
     public ResponseEntity<ProductOut> update(
         @PathVariable(required = true) Integer id,
         @RequestBody(required = true) ProductIn in
     );
 
+    @ApiOperation(value = "Get a product by id")
     @GetMapping("/products/{id}")
     public ResponseEntity<ProductOut> read(
         @PathVariable(required = true) Integer id
     );
 
+    @ApiOperation(value = "Get all products")
     @GetMapping("/products")
     ResponseEntity<List<ProductOut>> readAll();
 
